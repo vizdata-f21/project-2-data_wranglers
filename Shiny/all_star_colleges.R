25a9f4068b32390febd2fba6a2e9ca1c08332f59
Owen Henry <owen.henry@duke.edu>
2021-11-29 16:41:58 -0500

    made folder names lowercase; closed #13. removed README.html from data folder; closed #18

--- Shiny/all_star_colleges.R
+++ Shiny/all_star_colleges.R
@@ -1,134 +1,158 @@
-
+extrafont::loadfonts(device="win")
 library(tidyverse)
 library(gganimate)
 
-all_stars <- read_csv("../Data/all_star.csv", show_col_types = F) %>%
+
+
+all_stars <- read_csv("./Data/all_star.csv", show_col_types = F) %>%
   filter(!(year == 1999 & str_detect(draft_pick, "20")))
-colleges <- read_csv("../Data/colleges.csv", show_col_types = F)
-states <- read_csv("../Data/states.csv", show_col_types = F)
-stats <- read_csv("../Data/stats.csv", show_col_types = F)
+colleges <- read_csv("./Data/colleges.csv", show_col_types = F)
 
-colleges
-all_stars
+data <- right_join(colleges, all_stars)
 
-all_stars_w_colleges <- right_join(colleges, all_stars)
-
-all_stars_w_colleges %>%
+anim_w_duplicates <- data %>%
+  select(players, college, year) %>%
   filter(!is.na(college)) %>%
+  mutate(college = ifelse(str_detect(college, "Indiana, Georgetown"), "Georgetown", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Niagara"), "Niagara", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Midland College, Oklahoma"), "Oklahoma", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Bradley, New Mexico"), "Oklahoma", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Vincennes University, Michigan"), "Michigan", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Vincennes University, UNLV"), "UNLV", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Trinity Valley CC, Cincinnati"), "Cincinnati", college)) %>%
   select(college, year) %>%
-  arrange(year) %>%
-  group_by(college, year) %>%
-  mutate(rank_in_year = rank(college, ties.method = "first"))
-
-all_stars_w_colleges %>%
-  filter(!is.na(college)) %>%
-  select(college, year) %>%
-  arrange(year) %>%
   group_by(year) %>%
-  count(college)
-
-x1$cumsum <- ave(x1$n, x1$college, FUN=cumsum)
-
-x %>%
-  group_by(year) %>%
-  mutate(rank_in_year = rank(cumsum, ties.method = "first"))
-
-x1<-x %>%
-  complete(nesting(college, year), fill = list(frequency = 0)) %>% 
-  mutate(n = replace_na(n, 0))
-
-x1$cumsum <- ave(x1$n, x1$college, FUN=cumsum)
-
-x2 <- x1 %>%
-  group_by(year) %>%
-  mutate(rank_in_year = rank(cumsum, ties.method = "first"))
-
-colleges_anim <- x2 %>%
+  count(college) %>% 
+  pivot_wider(
+    names_from = college,
+    values_from = n
+  ) %>%
+  pivot_longer(-1, 
+               names_to = "college",
+               values_to = "n") %>% 
+  mutate(n = replace_na(n, 0)) %>%
+  group_by(college) %>%
+  mutate(sum_all_stars = cumsum(n)) %>%
   ungroup() %>%
-  ggplot(aes(x = cumsum, y = factor(rank_in_year))) +
-  geom_col(aes(fill = college), show.legend = F) +
-  facet_wrap(~year) +
-  facet_null() +
-  coord_cartesian(clip = "off") +
-  theme(
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    plot.margin = margin(1,0,0,6, "cm"),
-    axis.text.x = element_text(size=15),
-    plot.title = element_text(size=15),
-    plot.title.position = "plot",
-    plot.subtitle = element_text(size=12)
+  group_by(year) %>%  
+  arrange(year, -sum_all_stars) %>%
+  mutate(rank = 1:n()) %>%  
+  filter(rank <= 10) %>%
+  ggplot() +  
+  aes(xmin = 0 ,  
+      xmax = sum_all_stars) +  
+  aes(ymin = rank - .45,  
+      ymax = rank + .45,  
+      y = rank) +  
+  facet_wrap(~ year) +  
+  geom_rect(alpha = .7, show.legend = F) +  
+  aes(fill = college) +  
+  scale_fill_viridis_d(option = "magma",  
+                       direction = -1) +
+  scale_x_continuous(
+    limits = c(-10, 40),
+    breaks = c(5*(0:8))
   ) +
   geom_text(
     hjust = "right",
     aes(label = college),
-    x = -10, size = 5
+    x = -0.5, size = 5
+  ) +
+  theme_classic(base_family = "Times") +
+  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), 
+        axis.line.y = element_blank(), legend.background = element_rect(fill = "gainsboro"), 
+        plot.background = element_rect(fill = "gainsboro"),
+        panel.background = element_rect(fill = "gainsboro")) +  
+  scale_y_reverse() +
+  labs(fill = NULL, x = "All-Stars", y = NULL) +  
+  facet_null() +
+  scale_x_continuous(
+    limits = c(-10, 40),
+    breaks = c(5*(0:8))
   ) +
   geom_text(
-    x = 275, y = 2,
-    hjust = "left",
+    x = 30, y = -10,
     aes(label = as.character(year)),
-    size = 10
+    size = 25,  
+    family = "Times"
   ) +
-  aes(group = college) +
-  transition_time(as.integer(year))
+  aes(group = college) +  
+  gganimate::transition_time(year)
 
-animate(colleges_anim, end_pause = 20)
+animate(anim_w_duplicates, end_pause = 20, nframes = 200)
 
-all_stars_w_colleges %>%
+
+
+
+
+anim_no_duplicates <- data %>%
+  select(players, college, year) %>%
+  filter(!is.na(college)) %>%
+  mutate(college = ifelse(str_detect(college, "Indiana, Georgetown"), "Georgetown", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Niagara"), "Niagara", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Midland College, Oklahoma"), "Oklahoma", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Bradley, New Mexico"), "Oklahoma", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Vincennes University, Michigan"), "Michigan", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Vincennes University, UNLV"), "UNLV", college)) %>%
+  mutate(college = ifelse(str_detect(college, "Trinity Valley CC, Cincinnati"), "Cincinnati", college)) %>%
+  filter(duplicated(players, college) == FALSE) %>%
   select(college, year) %>%
-  group_by(college, year) %>%
-  mutate(rank_in_year = rank(cumsum, ties.method = "first")) %>%
-  ungroup() %>%
-  ggplot(aes(x = cumsum, y = factor(rank_in_year))) +
-  geom_col(aes(fill = college), show.legend = F) +
-  facet_wrap(~year)
-  
-  
-  
-all_stars_w_colleges %>%
-  select(-country_code, -indicator_name, -indicator_code) %>%
-  pivot_longer(!country_name, names_to = "year", values_to = "population") %>%
-  mutate(year = as.numeric(str_replace(year, "x", ""))) %>%
   group_by(year) %>%
-  mutate(rank_in_year = rank(population, ties.method = "first")) %>%
+  count(college) %>% 
+  pivot_wider(
+    names_from = college,
+    values_from = n
+  ) %>%
+  pivot_longer(-1, 
+               names_to = "college",
+               values_to = "n") %>% 
+  mutate(n = replace_na(n, 0)) %>%
+  group_by(college) %>%
+  mutate(sum_all_stars = cumsum(n)) %>%
   ungroup() %>%
-  ggplot(aes(x = population, y = factor(rank_in_year))) +
-  geom_col(aes(fill = country_name), show.legend = F) +
-  facet_wrap(~year) +
-  scale_x_continuous(labels = unit_format(unit = "B", scale = 1e-9, accuracy = 0.1),
-                     limits = c(0, 1750000000), 
-                     expand = c(0, 0)) +
-  theme(
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    plot.margin = margin(1,0,0,6, "cm"),
-    axis.text.x = element_text(size=15),
-    plot.title = element_text(size=15),
-    plot.title.position = "plot",
-    plot.subtitle = element_text(size=12)
+  group_by(year) %>%  
+  arrange(year, -sum_all_stars) %>%
+  mutate(rank = 1:n()) %>%  
+  filter(rank <= 10) %>%
+  ggplot() +  
+  aes(xmin = 0 ,  
+      xmax = sum_all_stars) +  
+  aes(ymin = rank - .45,  
+      ymax = rank + .45,  
+      y = rank) +  
+  facet_wrap(~ year) +  
+  geom_rect(alpha = .7, show.legend = F) +  
+  aes(fill = college) +  
+  scale_fill_viridis_d(option = "magma",  
+                       direction = -1) +
+  scale_x_continuous(
+    limits = c(-5, 16),
+    breaks = c(2*(0:8))
   ) +
-  coord_cartesian(clip = "off") +
   geom_text(
     hjust = "right",
-    aes(label = country_name),
-    x = -20000000, size = 5
+    aes(label = college),
+    x = -0.5, size = 5
   ) +
-  labs(x = NULL, y = NULL, title = "Populations of most populous countries", subtitle = "1961 to 2020") +
+  theme_classic(base_family = "Times") +
+  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), 
+        axis.line.y = element_blank(), legend.background = element_rect(fill = "gainsboro"), 
+        plot.background = element_rect(fill = "gainsboro"),
+        panel.background = element_rect(fill = "gainsboro")) +  
+  scale_y_reverse() +
+  labs(fill = NULL, x = "All-Stars", y = NULL) +  
   facet_null() +
-  geom_text(
-    x = 1040000000, y = 1.5,
-    hjust = "left",
-    aes(label = as.character(year)),
-    size = 10
+  scale_x_continuous(
+    limits = c(-4.5, 16),
+    breaks = c(2*(0:8))
   ) +
-  aes(group = country_name) +
-  transition_time(as.integer(year))
+  geom_text(
+    x = 12, y = -10,
+    aes(label = as.character(year)),
+    size = 25,  
+    family = "Times"
+  ) +
+  aes(group = college) +  
+  gganimate::transition_time(year)
 
-
+animate(anim_no_duplicates, end_pause = 20, nframes = 200)
