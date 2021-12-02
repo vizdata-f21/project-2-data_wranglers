2afdca30e57dcae504edd34d48d73b8b67819ee3
Owen Henry <owen.henry@duke.edu>
2021-12-02 00:37:39 -0500

    shiny

--- Shiny.R
+++ Shiny.R
@@ -13,11 +13,11 @@ source("shiny/college_locations.R")
 
 
 ui <- fluidPage(
-  title = "NBA Player Analysis",
+  title = "NBA All-Star Analysis",
   
   
   dashboardPage(skin = "green",
-                dashboardHeader(title = "NBA",
+                dashboardHeader(title = "NBA All-Stars",
                                 titleWidth = '100%'),
                 dashboardSidebar(
                   width = 150,
@@ -39,10 +39,11 @@ ui <- fluidPage(
                         nav_bar_color = 'black',
                         material_row(
                             material_card(
-                              title = '',
-                              depth = 4,
-                              radioButtons("duplicate_players", "Duplicate Players?",
-                                           choices = c("Yes" = T, "No" = F), selected = T),
+                              title = NULL,
+                              depth = 6,
+                              radioButtons("duplicate_players", "What would you like to count?",
+                                           choices = c("Total All-Star Game appearances by players" = T,
+                                                       "Number of players who have made an All-Star Game" = F), selected = T),
                               sliderInput("year_range_by_college", "Year Range",
                                           min = 1951, max = 2021, value = c(1951, 2021), sep = "", ticks = FALSE),
                               sliderInput("colleges_to_rank", "Colleges to Rank:",
@@ -53,7 +54,7 @@ ui <- fluidPage(
                                           value = 10, min = 5, max = 40, step = 5, ticks = FALSE),
                               sliderTextInput("end_pause_anim_by_college_in", "Pause at End of Animation:",
                                           choices = c("Short", "Medium", "Long"), selected = "Medium"),
-                              actionButton("animate_by_college", "Animate!")
+                              submitButton("Animate!")
                           ),
                           material_row(
                             imageOutput("plot_anim_by_college")
@@ -73,22 +74,21 @@ ui <- fluidPage(
                             radioButtons("per_capita", "Per million residents?",
                                          choices = c("Yes" = T, "No" = F), selected = T),
                             sliderInput("year_range_by_state_loc", "Year Range",
-                                        min = 1951, max = 2021, value = c(1951, 2021), sep = "", ticks = FALSE)
+                                        min = 1951, max = 2021, value = c(1951, 2021), sep = "", ticks = FALSE),
+                            submitButton("Plot!")
                           )
                         ),
                         material_row(
                           plotOutput("plot_result_by_state")
                         )
                       )
-                    )
-                    ,
+                    ),
                     
                     tabItem(
                       tabName = "as_loc",
                       material_page(
                         nav_bar_color = 'black',
                         material_row(
-                          material_column(
                             width = 4,
                             material_card(
                               title = '',
@@ -101,68 +101,11 @@ ui <- fluidPage(
                                           value = 10, min = 5, max = 40, step = 5, ticks = FALSE),
                               sliderInput("end_pause_anim_loc", "End Pause:",
                                           value = 25, min = 5, max = 100, step = 5, ticks = FALSE),
-                              actionButton("anim_by_college_loc", "Animate!")
+                              submitButton("Animate!")
                             )
                           ),
-                          material_column(
+                          material_row(
                             imageOutput("plot_anim_by_college_loc")
-                          )
-                        )
-                      )
-                    ),
-                    tabItem(
-                      tabName = "as_draft",
-                      material_page(
-                        nav_bar_color = 'black',
-                        material_row(
-                          material_column(
-                            width = 4,
-                            material_card(
-                              title = '',
-                              depth = 4,
-                              sliderInput("year_range_by_draft", "Year Range",
-                                          min = 1951, max = 2021, value = c(1951, 2021), sep = "", ticks = FALSE),
-                              sliderInput("duration_anim_by_draft", "Duration:",
-                                          value = 10, min = 5, max = 30, step = 5, ticks = FALSE),
-                              sliderInput("fps_anim_by_draft", "Frames per Second:",
-                                          value = 10, min = 5, max = 40, step = 5, ticks = FALSE),
-                              sliderInput("end_pause_anim_by_draft", "End Pause:",
-                                          value = 25, min = 5, max = 100, step = 5, ticks = FALSE),
-                              actionButton("animate_by_draft", "Animate!")
-                            )
-                          ),
-                          material_column(
-                            imageOutput("plot_anim_by_draft")
-                          )
-                        )
-                      )
-                    ),
-                    
-                    tabItem(
-                      tabName = "as_team",
-                      material_page(
-                        nav_bar_color = 'black',
-                        material_row(
-                          material_column(
-                            width = 4,
-                            material_card(
-                              title = '',
-                              depth = 4,
-                              sliderInput("year_range_by_team", "Year Range",
-                                          min = 1951, max = 2021, value = c(1951, 2021), sep = "", ticks = FALSE),
-                              sliderInput("teams_to_rank", "Teams to Rank (with the most All-Stars):",
-                                          value = 30, min = 10, max = 30, ticks = FALSE),
-                              sliderInput("duration_anim_by_team", "Duration:",
-                                          value = 10, min = 5, max = 30, step = 5, ticks = FALSE),
-                              sliderInput("fps_anim_by_team", "Frames per Second:",
-                                          value = 10, min = 5, max = 40, step = 5, ticks = FALSE),
-                              sliderInput("end_pause_anim_by_team", "End Pause:",
-                                          value = 25, min = 5, max = 100, step = 5, ticks = FALSE),
-                              actionButton("animate_by_draft", "Animate!")
-                            )
-                          ),
-                          material_column(
-                            imageOutput("plot_anim_by_team")
                           )
                         )
                       )
@@ -171,19 +114,23 @@ ui <- fluidPage(
                   )
                 )
   )
-)
+
 
 
 
 
 server <- function(input, output, session) {
   
-  # want to use eventReactive
-  observeEvent(input$animate_by_college, {
+  
+
+# ALL STARS BY COLLEGE ----------------------------------------------------
+
+  
     output$plot_anim_by_college <- renderImage({
       all_stars_by_college <- tempfile(fileext='.gif')
       
-      all_stars_by_college_anim <- number_of_all_stars_by_college(year_start = input$year_range_by_college[[1]],
+      all_stars_by_college_anim <- number_of_all_stars_by_college(duplicate_players = input$duplicate_players, 
+                                                                  year_start = input$year_range_by_college[[1]],
                                                                   year_end = input$year_range_by_college[[2]])
       
       if(input$end_pause_anim_by_college_in == "Short"){
@@ -205,15 +152,22 @@ server <- function(input, output, session) {
       list(src = "all_stars_by_college.gif",
            contentType = 'image/gif'
       )}, deleteFile = TRUE)
-  })
   
+    
+
+# ALL STARS BY COLLEGE STATE ----------------------------------------------
+
+    
   output$plot_result_by_state <- renderPlot({
     all_stars_by_state(per_capita = input$per_capita, year_start = input$year_range_by_state_loc[[1]],
                        year_end = input$year_range_by_state_loc[[2]])
   })
   
+
+# ALL STARS BY COLLEGE LOCATION -------------------------------------------
+
   
-  observeEvent(input$anim_by_college_loc, {
+  
     output$plot_anim_by_college_loc <- renderImage({
       all_stars_by_location <- tempfile(fileext='.gif')
       
@@ -229,40 +183,6 @@ server <- function(input, output, session) {
       list(src = "all_stars_by_location.gif",
            contentType = 'image/gif'
       )}, deleteFile = TRUE)
-  })
-  
-  observeEvent(input$anim_by_draft, {
-    output$plot_anim_by_draft <- renderImage({
-      all_stars_by_draft <- number_of_all_stars_by_draft(year_start = input$year_range_by_draft[[1]],
-                                                         year_end = input$year_range_draft[[2]])
-      
-      anim_save("all_stars_by_draft.gif", animate(all_stars_by_draft,
-                                                  fps = input$fps_anim_by_draft,
-                                                  end_pause = input$end_pause_anim_by_draft,
-                                                  duration = input$duration_anim_by_draft))
-      
-      list(src = "all_stars_by_draft.gif",
-           contentType = 'image/gif'
-      )}, deleteFile = TRUE)
-    
-  }) 
-  
-  observeEvent(input$anim_by_team, {
-    output$plot_anim_by_team <- renderImage({
-      all_stars_by_team <- number_of_all_stars_by_team(year_start = input$year_range_by_team[[1]],
-                                                       year_end = input$year_range_team[[2]])
-      
-      anim_save("all_stars_by_team.gif", animate(all_stars_by_team,
-                                                 fps = input$fps_anim_by_team,
-                                                 end_pause = input$end_pause_anim_by_team,
-                                                 duration = input$duration_anim_by_team))
-      
-      list(src = "all_stars_by_team.gif",
-           contentType = 'image/gif'
-      )}, deleteFile = TRUE)
-    
-  }) 
-  
   
 }
 
