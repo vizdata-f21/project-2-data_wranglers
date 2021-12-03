4469e82a7f50b5234c1943502c1fe0929a25471e
Sarab Bhasin <bhasin.sarab@gmail.com>
2021-12-03 00:30:28 -0500

    title change

--- Shiny.R
+++ Shiny.R
@@ -212,7 +212,7 @@ ui <- fluidPage(
                     ),
                     tabItem(
                       tabName = "world_map",
-                      h1("All-Stars by Country"),
+                      h1("All-Stars by Birth Country"),
                       sidebarLayout(
                         position = "right",
                         sidebarPanel(
